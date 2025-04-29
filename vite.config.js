388ddbbc2cb68f4d22643aee9d5428a2f4ff178f
hosain ali <hosainahmed534745@gmail.com>
2025-04-29 12:41:03 +0600

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,4 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: {
-    host: '10.0.60.24',
-    port: 5173,
-  },
 });
